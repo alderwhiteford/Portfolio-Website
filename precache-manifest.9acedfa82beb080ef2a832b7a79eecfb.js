cd38e14ac1fe6f5e6c0e02021a122ef0b7c4d292
alderwhiteford <whiteford.a@northeastern.edu>
2023-01-14 17:52:54 -0500

    Updates

---
+++ precache-manifest.9acedfa82beb080ef2a832b7a79eecfb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9c5cc238413ed13dcf",
+    "url": "/Portfolio-Website/static/css/main.7193f7f0.chunk.css"
+  },
+  {
+    "revision": "8d9c5cc238413ed13dcf",
+    "url": "/Portfolio-Website/static/js/main.8d9c5cc2.chunk.js"
+  },
+  {
+    "revision": "8dc86d05632e6855daa1",
+    "url": "/Portfolio-Website/static/css/1.fbe2e70b.chunk.css"
+  },
+  {
+    "revision": "8dc86d05632e6855daa1",
+    "url": "/Portfolio-Website/static/js/1.8dc86d05.chunk.js"
+  },
+  {
+    "revision": "9b6b17faf27c71f0d4ca",
+    "url": "/Portfolio-Website/static/js/runtime~main.9b6b17fa.js"
+  },
+  {
+    "revision": "7f281199258d96e249a7fce4101006b9",
+    "url": "/Portfolio-Website/static/media/Helvetica Neu Bold.7f281199.ttf"
+  },
+  {
+    "revision": "40878d85989fd728da121cf85281e981",
+    "url": "/Portfolio-Website/static/media/SpaceMono-Bold.40878d85.ttf"
+  },
+  {
+    "revision": "96985f7a507afce5ab786569d2b2368f",
+    "url": "/Portfolio-Website/static/media/SpaceMono-Regular.96985f7a.ttf"
+  },
+  {
+    "revision": "0facaae97183b8fede52099930aefd8d",
+    "url": "/Portfolio-Website/static/media/HelveticaNeue Light.0facaae9.ttf"
+  },
+  {
+    "revision": "0a13c540938b1b7dd3996b02ea568e5f",
+    "url": "/Portfolio-Website/static/media/HelveticaNeue Medium.0a13c540.ttf"
+  },
+  {
+    "revision": "78c28465643a20597ce65eee037a7675",
+    "url": "/Portfolio-Website/static/media/HelveticaNeue Thin.78c28465.ttf"
+  },
+  {
+    "revision": "4ff686ee78ff095848014f4283f67a41",
+    "url": "/Portfolio-Website/static/media/HelveticaNeue BlackCond.4ff686ee.ttf"
+  },
+  {
+    "revision": "f878563f02eabceaaf312536029a765a",
+    "url": "/Portfolio-Website/index.html"
+  }
+];
