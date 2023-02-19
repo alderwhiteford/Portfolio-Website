d13b0b7ba9b4ac0b0a333e0a33d2324131c8f921
alderwhiteford <whiteford.a@northeastern.edu>
2023-02-18 23:40:45 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio-Website/precache-manifest.5d5ec232a7981c1a0d997ab4eae79c94.js"
+  "/Portfolio-Website/precache-manifest.8acff839c73c77cb202ef647e71ea38c.js"
 );
 
 workbox.clientsClaim();
